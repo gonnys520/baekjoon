30c2b1f7566d035c2b5fd222fbf9a8f0f7dc4757
gonnys520 <gonnys520@gmail.com>
2019-02-10 17:41:35 +0900

    1546, 4344 추가

---
+++ src/org/gonnys/Question4344.java
@@ -0,0 +1,21 @@
+package org.gonnys;
+
+import java.util.Scanner;
+
+public class Question4344 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int c = sc.nextInt();
+		int n, total, count;
+		double avg;
+		int scores[] = new int[1000];
+		
+		
+		
+		
+		
+	}
+}
